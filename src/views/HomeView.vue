7a21dc24100b706aeeac0bc31df54096a075272e
Koch2333 <Koch233333@gmail.com>
2024-12-01 14:22:51 +0800

    A version for heartbeat club (#2)
    Merge the Heartbeat Club’s version into the heartbeat branch.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,33 @@
 <script setup lang="ts">
-import Logo from "@/assets/logo.svg"
 import GithubIcon from "@/assets/Github_Logo.svg"
 import BiliIcon from "@/assets/BiliBili_TV_Icon.svg"
-import { Mail } from "lucide-vue-next"
+import TiktokIcon from "@/assets/tiktok.svg"
 </script>
 
 <template>
   <main>
     <div class="overview">
       <div class="logo">
-        <Logo></Logo>
+        <img src="@/assets/logo.png" id="logo" weight="360" height="360"/>
       </div>
       <div class="divider"></div>
       <div class="introduction">
-        <h1 class="name">原子信息社</h1>
-        <i>ATOM IT CLUB</i>
+        <h1 class="name">HeartBeat 心乐社</h1>
+        <i>HEARTBEAT CLUB</i>
+        <p>用心倾乐，乐涌于心。</p>
         <div class="links">
-          <a href="https://bilibili.com">
+          <a href="https://space.bilibili.com/1043671230">
             <BiliIcon class="bili"></BiliIcon>
           </a>
-          <a href="mailto:chengruichen3@gmail.com">
-            <Mail></Mail>
-          </a>
-          <a href="https://github.com/atomclub">
-            <GithubIcon viewBox="0 0 25 25" stroke="currentColor" fill="currentColor"></GithubIcon>
-          </a>
+          <a><TiktokIcon></TiktokIcon></a>
         </div>
-
       </div>
     </div>
   </main>
+  <p>HeartBeat心乐社创立于2012年，是潮实高中部的第二批社团之一，也是校内唯一的大型音乐社团。<br></p>
+  <p>HeartBeat心乐社是一个充满激情与创造力的社团。我们致力于提供一个平台，让全校的音乐爱好者们能够汇聚一堂，共同学习、交流和分享音乐的魅力。
+    <br><br>分部HeartBeat心乐社包含歌手部、乐手部和企划部三个部门。三个部门各司其职，共同组成了HeartBeat心乐社。</p>
 
-  <p>原子信息社创立于2024年，是由一群对信息技术充满热情的潮阳实验学校普宁校区学生组成的社团。我们不仅仅是编程爱好者，我们是技术的探索者，是创新的实践者。包含技术组、协同组两个部门。两个部门各司其职，共同组成了原子信息社。</p>
-  <ul>
-    <li>
-      技术提升：与志同道合的伙伴一起，你的技术将得到飞速提升。
-    </li>
-    <li>
-      项目经验：参与实际项目，让你的简历更加丰富。
-    </li>
-    <li>
-      思维碰撞：与高手过招，激发你的创新思维。
-    </li>
-  </ul>
 </template>
 
 <style scoped>
@@ -103,7 +88,7 @@ ul {
   color: rgb(0 179 250);
 }
 
-@media screen and (max-width: 700px) {
+@media screen and (max-width: 900px) {
   .overview {
     display: block;
   }
@@ -116,14 +101,14 @@ ul {
     margin: 1rem 0;
   }
 
-  .logo svg {
-    margin: auto;
-    border-radius: 50%;
-    width: 5rem;
-    height: 5rem;
+  .logo img {
     display: block;
-
+    width: 110px;
+    align-items: center;
+    height: auto;
+    margin: 0 auto;
   }
 
 }
+
 </style>
